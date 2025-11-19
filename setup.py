a76e99341f718f7855a6a89d1d40f2210210661f
czilialadar <cili.aladar@icloud.com>
2025-11-19 12:46:03 +0100

    launch split

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/ftg.launch.xml']),
+        ('share/' + package_name + '/launch', ['launch/ftg_labirinth.launch.xml']),
+        ('share/' + package_name + '/launch', ['launch/ftg_box.launch.xml']),
         ('share/' + package_name + '/rviz', ['rviz/ftg.rviz'])
     ],
     install_requires=['setuptools'],
