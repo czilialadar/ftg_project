575da0195935e8aab035c58c70a553aa550672be
czilialadar <cili.aladar@icloud.com>
2025-12-02 11:07:08 +0100

    parameters file fixed

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         ('share/' + package_name + '/launch', ['launch/ftg_labirinth.launch.xml']),
         ('share/' + package_name + '/launch', ['launch/ftg_box.launch.xml']),
         ('share/' + package_name + '/launch', ['launch/ftg_test.launch.xml']),
+        ('share/' + package_name + '/config', ['config/params.yaml']),
         ('share/' + package_name + '/rviz', ['rviz/ftg.rviz'])
     ],
     install_requires=['setuptools'],
